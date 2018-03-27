e044193bfb3806a4a6b6523a768659681a750e07
Lingga Edo <lingga.edo@1rstwap.com>
2018-03-27 10:55:20 +0700

    fix gradle invoice project

--- BBW-Pulsa/src/main/java/bbw/pulsa/dao/PulsaDaoImpl.java
+++ BBW-Pulsa/src/main/java/bbw/pulsa/dao/PulsaDaoImpl.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 

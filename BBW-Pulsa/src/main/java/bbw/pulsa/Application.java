78c0689e3eb807ae6561570623626affa9d1d9e1
linggaaskaedo <lemp.otis@gmail.com>
2018-04-07 23:41:54 +0700

    make spring boot become war

--- BBW-Pulsa/src/main/java/bbw/pulsa/Application.java
+++ BBW-Pulsa/src/main/java/bbw/pulsa/Application.java
@@ -2,16 +2,22 @@ package bbw.pulsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Lingga on 12/03/18.
  */
 
 @SpringBootApplication
-public class Application
+public class Application extends SpringBootServletInitializer
 {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class);

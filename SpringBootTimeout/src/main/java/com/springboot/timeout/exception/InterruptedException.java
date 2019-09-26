7509d7587623ce89afe18d196b35e1d6b9d98db0
Lingga Putra (ID) <lingga.putra@Lingga-MACC.local>
2019-09-26 17:27:48 +0700

    add sample Callable Interrupt

---
+++ SpringBootTimeout/src/main/java/com/springboot/timeout/exception/InterruptedException.java
@@ -0,0 +1,11 @@
+package com.springboot.timeout.exception;
+
+public class InterruptedException extends RuntimeException
+{
+	private static final long serialVersionUID = 8787163997032616317L;
+
+	public InterruptedException()
+    {
+        // Do nothing
+    }
+}
